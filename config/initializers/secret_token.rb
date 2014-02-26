801c75b4fb3d7d3a17f9ef160f934e846c7b0c4a
dbushy727 <d.bushkanets@gmail.com>
2014-02-25 20:48:25 -0500

    initiated rails project

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Psyfin::Application.config.secret_key_base = '0d677e56dc1d45b207bee36da4ce300b00a87a6c37b3384377e34244a3f8b2045f13087801b13db1684837f920903a5c77531ae75fbe53d100b993a087679d4a'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Psyfin::Application.config.session_store :cookie_store, key: '_psyfin_session'
